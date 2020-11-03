1f534cfe5a3175fb53bc39b45fa017880368c5fa
U-LAPTOP-Q8376KNG\lucan <luca.nurmio.001@student.uni.lu>
2020-11-03 18:50:25 +0100

    Modify yagoFacts search code to make several DFs and merge them

--- yagoFactsSearchEngine.py
+++ yagoFactsSearchEngine.py
@@ -1,37 +1,31 @@
 import pandas as pd
 queryList = []
-mainIndexList = []
-importance = []
-predicateListFrequencies = []
-#                       Get search terms
-while "" not in queryList:
-        queryList.append(input('Please add an element to your query or just hit enter: '))
+df3 = pd.DataFrame()
+while [] not in queryList:
+        queryList.append(input('Please enter your query or press enter: ').split())
 queryList.pop()
-#                       Create pandas dataframe
 df = pd.read_csv('yagoFactsCleaned.csv')
 df.columns = ['Subject', 'Predicate', 'Object']
-#                       Get matching indices (as opposed to the rows themselves along with their information)
-for i in range(0, len(queryList)):
-    mainIndexList.extend(df.index[df.loc[:, "Subject"] == queryList[i]].tolist())
-    mainIndexList.extend(df.index[df.loc[:, "Predicate"] == queryList[i]].tolist())
-    mainIndexList.extend(df.index[df.loc[:, "Object"] == queryList[i]].tolist())
-mainIndexListUnique = list(dict.fromkeys(mainIndexList))
-#                       Get match occurences (importance/relevance) per index
-for m in mainIndexListUnique:
-        importance.append(100**mainIndexList.count(m))
-#                       Create new table from matching indices
-df2 = df.iloc[mainIndexListUnique]
-#                       Create new column in new table for matching indices and their match occurence score (i.e. mainIndexListUnique and importance)
-df2.loc[:, "Relevance"] = importance
-#                       Count frequency of rarest predicate in new table
-predicateList = df2.loc[:, 'Predicate'].tolist()
-predicateListUnique = list(dict.fromkeys(predicateList))
-for k in predicateList:
-        predicateListFrequencies.append(predicateList.count(k))
-lowestUniquePredicateFrequency = min(predicateListFrequencies)
-#                       For the new table: Divide importance/relevance score of rows by frequency of their respective predicates, then multiply it by frequency of the rarest predicate
-for n in predicateListUnique:
-        df2.loc[df.loc[:, 'Predicate'] == n, 'Relevance'] = (df2.loc[df.loc[:, 'Predicate'] == n, 'Relevance']/predicateList.count(n)) * lowestUniquePredicateFrequency
-#                       Print the sorted new table
-df2 = df2.sort_values(by=['Relevance'], ascending=False)
-print(df2.head(10))
+for l in range(0, len(queryList)):
+        mainIndexList = []
+        importance = []
+        predicateListFrequencies = []
+        for i in range(0, len(queryList[l])):
+            mainIndexList.extend(df.index[df.loc[:, "Subject"] == queryList[l][i]].tolist())
+            mainIndexList.extend(df.index[df.loc[:, "Predicate"] == queryList[l][i]].tolist())
+            mainIndexList.extend(df.index[df.loc[:, "Object"] == queryList[l][i]].tolist())
+        mainIndexListUnique = list(dict.fromkeys(mainIndexList))
+        for m in mainIndexListUnique:
+                importance.append(100**mainIndexList.count(m))
+        df2 = df.iloc[mainIndexListUnique]
+        df2.loc[:, "Relevance"] = importance
+        predicateList = df2.loc[:, 'Predicate'].tolist()
+        predicateListUnique = list(dict.fromkeys(predicateList))
+        for k in predicateList:
+                predicateListFrequencies.append(predicateList.count(k))
+        lowestUniquePredicateFrequency = min(predicateListFrequencies)
+        for n in predicateListUnique:
+                df2.loc[df.loc[:, 'Predicate'] == n, 'Relevance'] = (df2.loc[df.loc[:, 'Predicate'] == n, 'Relevance']/predicateList.count(n)) * lowestUniquePredicateFrequency
+        df3 = df3.append(df2)
+df3 = df3.sort_values(by=['Relevance'], ascending=False)
+print(df3.head(10))
